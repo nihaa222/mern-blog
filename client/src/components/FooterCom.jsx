deb7266264d31d6075eda5facef426ec7fe713fa
Niharika Deb <127780989+nihaa222@users.noreply.github.com>
2024-05-20 20:45:38 +0530

    footer

--- client/src/components/FooterCom.jsx
+++ client/src/components/FooterCom.jsx
@@ -27,32 +27,14 @@ export default function FooterCom() {
             <div>
               <Footer.Title title="About" />
               <Footer.LinkGroup col>
-                <Footer.Link
-                  href="https://www.100jsprojects.com"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  100 JS Projects
-                </Footer.Link>
-                <Footer.Link
-                  href="/about"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Bush Blog
-                </Footer.Link>
+                <Footer.Link>Projects</Footer.Link>
+                <Footer.Link>Bush Blog</Footer.Link>
               </Footer.LinkGroup>
             </div>
             <div>
               <Footer.Title title="Follow us" />
               <Footer.LinkGroup col>
-                <Footer.Link
-                  href="https://www.github.com/sahandghavidel"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Github
-                </Footer.Link>
+                <Footer.Link>Github</Footer.Link>
                 <Footer.Link href="#">Discord</Footer.Link>
               </Footer.LinkGroup>
             </div>
@@ -69,17 +51,14 @@ export default function FooterCom() {
         <div className="w-full sm:flex sm:items-center sm:justify-between">
           <Footer.Copyright
             href="#"
-            by="Sahand's blog"
+            by="Bush blog"
             year={new Date().getFullYear()}
           />
           <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
             <Footer.Icon href="#" icon={BsFacebook} />
             <Footer.Icon href="#" icon={BsInstagram} />
             <Footer.Icon href="#" icon={BsTwitter} />
-            <Footer.Icon
-              href="https://github.com/sahandghavidel"
-              icon={BsGithub}
-            />
+            <Footer.Icon icon={BsGithub} />
             <Footer.Icon href="#" icon={BsDribbble} />
           </div>
         </div>
